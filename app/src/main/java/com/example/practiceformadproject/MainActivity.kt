46b4ad0eb5ab1ca05ef07623a4b898bfbdb588e6
Pevinya <90928087+Pevinya@users.noreply.github.com>
2023-05-06 17:43:23 +0530

    main with delivery button

--- app/src/main/java/com/example/practiceformadproject/MainActivity.kt
+++ app/src/main/java/com/example/practiceformadproject/MainActivity.kt
@@ -35,11 +35,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-//        val fifthAct = findViewById<Button>(R.id.btnDelivery)
-//        fifthAct.setOnClickListener{
-//            val intent = Intent(this,FirstPage::class.java)
-//            startActivity(intent)
-//        }
+        val fifthAct = findViewById<Button>(R.id.btnDelivery)
+        fifthAct.setOnClickListener{
+            val intent = Intent(this,FirstPage::class.java)
+            startActivity(intent)
+       }
     }
 
 
