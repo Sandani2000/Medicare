a0d9bb143207b84e79956eb796ddfbf387b2c0df
Sandani Thilakarathna <127828782+Sandani2000@users.noreply.github.com>
2023-05-06 14:22:17 +0530

    Update medicines changes

--- app/src/main/java/com/example/practiceformadproject/EditMedicine.kt
+++ app/src/main/java/com/example/practiceformadproject/EditMedicine.kt
@@ -5,15 +5,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class EditMedicine : AppCompatActivity() {
+    private lateinit var backIcon: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_medicine)
 
-        val secondAct = findViewById<Button>(R.id.btnUpdate)
-        secondAct.setOnClickListener{
-            val intent = Intent(this,ServiceProviderViewMedicine::class.java)
+        //Initialize Imageview
+        backIcon = findViewById(R.id.backArrow)
+
+        //Navigate to All medicine recycle view after clicking back arrow
+        backIcon.setOnClickListener{
+            val intent = Intent(this, ServiceProviderRecyclerView::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/practiceformadproject/ServiceProviderRecyclerView.kt
+++ app/src/main/java/com/example/practiceformadproject/ServiceProviderRecyclerView.kt
@@ -1,5 +1,6 @@
 package com.example.practiceformadproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,13 +10,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
 
 class ServiceProviderRecyclerView : AppCompatActivity() {
-    private lateinit var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>
     private lateinit var layoutManager: RecyclerView.LayoutManager
     private lateinit var recyclerView: RecyclerView
     private lateinit var medicineList: ArrayList <MedicineModel>
     private lateinit var firebase: DatabaseReference
 
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service_provider_recycler_view)
 
@@ -27,9 +27,6 @@ class ServiceProviderRecyclerView : AppCompatActivity() {
         medicineList = arrayListOf<MedicineModel>()
 
         getMedicines()
-
-//        adapter = RecyclerAdapter()
-//        recyclerView.adapter = adapter
     }
 
     private fun getMedicines() {
@@ -48,6 +45,28 @@ class ServiceProviderRecyclerView : AppCompatActivity() {
                     val mAdapter = RecyclerAdapter(medicineList)
                     recyclerView.adapter = mAdapter
 
+                    mAdapter.setOnItemClickListener(object : RecyclerAdapter.OnItemClickListener{
+                        override fun onItemClick(position: Int) {
+                            val intent = Intent(this@ServiceProviderRecyclerView, ServiceProviderViewMedicine::class.java)
+
+                            //put extras
+                            intent.putExtra("medicineId", medicineList[position].medId )
+                            intent.putExtra("medicineName", medicineList[position].name)
+                            intent.putExtra("medicineDescription",medicineList[position].description)
+                            intent.putExtra("medicineManufactureName",medicineList[position].manufactureName)
+                            intent.putExtra("medicineManufactureDate",medicineList[position].manufactureDate)
+                            intent.putExtra("medicineExpiryDate",medicineList[position].expiryDate)
+                            intent.putExtra("medicineQuantity",medicineList[position].quantity)
+                            intent.putExtra("medicinePrice",medicineList[position].pricePerUnit)
+                            intent.putExtra("medicineCategory",medicineList[position].category)
+                            startActivity(intent)
+
+//                            "Medicine Image and Pharmacy Name to be add"
+//                            intent.putExtra("pharmacyName",pharmacyList[position].pharmacyName)
+//                            intent.putExtra("medicineImage",medicineList[position].image)
+                        }
+
+                    })
                     recyclerView.visibility = View.VISIBLE
 
                 }
